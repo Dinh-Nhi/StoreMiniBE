9df1517549db5ab3ab245ecabb5dbbf3f199d3c5
ADMIN-PC\Admin <dinhnhi08it@gmail.com>
2025-08-26 20:09:12 +0700

    update create project

--- src/main/java/com/storemini/StoreMiniApplication.java
+++ src/main/java/com/storemini/StoreMiniApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TechPeakApplication {
+public class StoreMiniApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TechPeakApplication.class, args);
+        SpringApplication.run(StoreMiniApplication.class, args);
     }
 }
